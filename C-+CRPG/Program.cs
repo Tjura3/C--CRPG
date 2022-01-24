819f303b52084ac3aa4ac6ad419bc9097ac55a75
Thomas <1707235@mukilteo.wednet.edu>
2022-01-23 17:39:21 -0800

    add4ed small thing

--- C-+CRPG/Program.cs
+++ C-+CRPG/Program.cs
@@ -7,6 +7,7 @@ namespace C__CRPG
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("pressAnytoEXIT");
         }
     }
 }
