e35046d74cef3c72646ed5d69892cb6d384b32bf
Thomas <1707235@mukilteo.wednet.edu>
2022-01-23 17:36:36 -0800

    Add project files.

---
+++ C-+CRPG/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C__CRPG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
