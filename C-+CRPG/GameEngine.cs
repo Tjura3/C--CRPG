bf389813b9fb993901c4a6f19dc4f7cc7473e8e4
MUKILTEO\1707235 <1707235@mukilteo.wednet.edu>
2022-02-17 09:46:53 -0800

    Done with V4

--- C-+CRPG/GameEngine.cs
+++ C-+CRPG/GameEngine.cs
@@ -8,7 +8,9 @@ namespace C__CRPG
     { 
        
         public static string Version = "0.0.4";
+        public static Monster _currentMonster;
        
+
         public static void Initialize()
         {
             Console.ForegroundColor = ConsoleColor.DarkRed;
@@ -31,8 +33,17 @@ namespace C__CRPG
             World.ListMonsters();
             World.ListQuests();
             World.ListItems();
+            if (_currentMonster != null)
+            {
+                Console.WriteLine("Current Monster: {0}", _currentMonster.Name);
+            }
+            else Console.WriteLine("No current monster");
         }
-        
+
+
+
+
+        /*      old quest processor... why was I told to delete this?
         public static void QuestProcessor(Player player, Location location)
         {
             //does this location have a quest?
@@ -56,6 +67,113 @@ namespace C__CRPG
 
             }//location.QuestAvallibleHere
         }//quest processor
+        */
+
+
+
+        public static void QuestProcessor(Player _player, Location newLocation)
+        {
+            string questMessage;
+            // Does the location have a quest?
+            if (newLocation.QuestAvalibleHere != null)
+            {
+                // See if the player already has the quest, and if they've completed it
+                bool playerAlreadyHasQuest = _player.HasThisQuest(newLocation.QuestAvalibleHere);
+                bool playerAlreadyCompletedQuest = _player.CompletedThisQuest(newLocation.QuestAvalibleHere);
+
+                // See if the player already has the quest
+                if (playerAlreadyHasQuest)
+                {
+                    // If the player has not completed the quest yet
+                    if (!playerAlreadyCompletedQuest)
+                    {
+                        // See if the player has all the items needed to complete the quest
+                        bool playerHasAllItemsToCompleteQuest = _player.HasAllQuestCompletionItems(newLocation.QuestAvalibleHere);
+
+                        // The player has all items required to complete the quest
+                        if (playerHasAllItemsToCompleteQuest)
+                        {
+                            // Display message
+                            questMessage = Environment.NewLine;
+                            questMessage += "You complete the '" + newLocation.QuestAvalibleHere.Name + "' quest." + Environment.NewLine;
+
+                            // Remove quest items from inventory
+                            _player.RemoveQuestCompletionItems(newLocation.QuestAvalibleHere);
+
+                            // Give quest rewards
+                            questMessage += "You receive: " + Environment.NewLine;
+                            questMessage += newLocation.QuestAvalibleHere.RewardExperiencePoints.ToString() + " experience points" + Environment.NewLine;
+                            questMessage += newLocation.QuestAvalibleHere.RewardGold.ToString() + " gold" + Environment.NewLine;
+                            questMessage += newLocation.QuestAvalibleHere.RewardItem.Name + Environment.NewLine;
+                            questMessage += Environment.NewLine;
+                            Console.WriteLine(questMessage);
+
+                            _player.ExperiencePoints += newLocation.QuestAvalibleHere.RewardExperiencePoints;
+                            _player.Gold += newLocation.QuestAvalibleHere.RewardGold;
+
+                            // Add the reward item to the player's inventory
+                            _player.AddItemToInventory(newLocation.QuestAvalibleHere.RewardItem);
+
+                            // Mark the quest as completed
+                            _player.MarkQuestCompleted(newLocation.QuestAvalibleHere);
+                        }
+                    }
+                }
+                else
+                {
+                    // The player does not already have the quest
+
+                    // Display the messages
+                    questMessage = "You receive the " + newLocation.QuestAvalibleHere.Name + " quest." + Environment.NewLine;
+                    questMessage += newLocation.QuestAvalibleHere.Description + Environment.NewLine;
+                    questMessage += "To complete it, return with:" + Environment.NewLine;
+                    foreach (QuestCompletionItem qci in newLocation.QuestAvalibleHere.QuestCompletionItems)
+                    {
+                        if (qci.Quantity == 1)
+                        {
+                            questMessage += qci.Quantity.ToString() + " " + qci.Details.Name + Environment.NewLine;
+                        }
+                        else
+                        {
+                            questMessage += qci.Quantity.ToString() + " " + qci.Details.NamePlural + Environment.NewLine;
+                        }
+                    }
+                    questMessage += Environment.NewLine;
+                    Console.WriteLine(questMessage);
+
+                    // Add the quest to the player's quest list
+                    _player.Quests.Add(new PlayerQuest(newLocation.QuestAvalibleHere, false));
+                }
+            }
+
+        } //QuestProcessor
+
+
+        public static void MonsterProcessor(Player _player, Location newLocation)
+        {
+            string monsterMessage = " ";
+            if (newLocation.MonsterLivingHere != null)
+            {
+                monsterMessage += $"You see a {newLocation.MonsterLivingHere.Name}\n";
+                Console.WriteLine(monsterMessage);
+
+                //make a new monster
+                Monster standardMonster = World.MonsterByID(newLocation.MonsterLivingHere.ID);
+                _currentMonster = new Monster(standardMonster.ID, standardMonster.Name, standardMonster.MaxDamage, standardMonster.RewardExperiencePoints, standardMonster.RewardGold, standardMonster.CurrentHitPoints, standardMonster.MaximumHitPoints);
+
+                foreach(LootItem lootItem in standardMonster.LootTable)
+                {
+                    _currentMonster.LootTable.Add(lootItem);
+                }
+                
+            }else { _currentMonster = null; }
+
+
+
+
+        }//Monster Processor
+
+
 
     }
 }
--- C-+CRPG/Player.cs
+++ C-+CRPG/Player.cs
@@ -52,6 +52,7 @@ namespace C__CRPG
             CurrentHitPoints = (CurrentHitPoints >= MaximumHitPoints) ? CurrentHitPoints += 0 : CurrentHitPoints++;
             CurrentLocation = loc;
             GameEngine.QuestProcessor(this, loc);
+            GameEngine.MonsterProcessor(this, loc);
         }//move method
 
 
@@ -110,6 +111,119 @@ namespace C__CRPG
                 Console.WriteLine("You cant go there.");
             }
         }
+        #endregion
+
+        #region Coppied code in V003 03
+        ////////////////////////////
+        //this is where quests work
+        /////////////////////////////
+        public bool HasThisQuest(Quest quest)
+        {
+            foreach (PlayerQuest playerQuest in Quests)
+            {
+                if (playerQuest.Details.ID == quest.ID)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public bool CompletedThisQuest(Quest quest)
+        {
+            foreach (PlayerQuest playerQuest in Quests)
+            {
+                if (playerQuest.Details.ID == quest.ID)
+                {
+                    return playerQuest.IsCompleted;
+                }
+            }
+
+            return false;
+        }
+
+        public bool HasAllQuestCompletionItems(Quest quest)
+        {
+            // See if the player has all the items needed to complete the quest here
+            foreach (QuestCompletionItem qci in quest.QuestCompletionItems)
+            {
+                bool foundItemInPlayersInventory = false;
+
+                // Check each item in the player's inventory, to see if they have it, and enough of it
+                foreach (BasicItem bi in Inventory)
+                {
+                    if (bi.Details.ID == qci.Details.ID) // The player has the item in their inventory
+                    {
+                        foundItemInPlayersInventory = true;
+
+                        if (bi.Quantity < qci.Quantity) // The player does not have enough of this item to complete the quest
+                        {
+                            return false;
+                        }
+                    }
+                }
+
+                // The player does not have any of this quest completion item in their inventory
+                if (!foundItemInPlayersInventory)
+                {
+                    return false;
+                }
+            }
+
+            // If we got here, then the player must have all the required items, and enough of them, to complete the quest.
+            return true;
+        }
+
+        public void RemoveQuestCompletionItems(Quest quest)
+        {
+            foreach (QuestCompletionItem qci in quest.QuestCompletionItems)
+            {
+                foreach (BasicItem bi in Inventory)
+                {
+                    if (bi.Details.ID == qci.Details.ID)
+                    {
+                        // Subtract the quantity from the player's inventory that was needed to complete the quest
+                        bi.Quantity -= qci.Quantity;
+                        break;
+                    }
+                }
+            }
+        }
+
+
+        public void AddItemToInventory(Items itemToAdd)
+        {
+            foreach (BasicItem ii in Inventory)
+            {
+                if (ii.Details.ID == itemToAdd.ID)
+                {
+                    // They have the item in their inventory, so increase the quantity by one
+                    ii.Quantity++;
+
+                    return; // We added the item, and are done, so get out of this function
+                }
+            }
+
+            // They didn't have the item, so add it to their inventory, with a quantity of 1
+            Inventory.Add(new BasicItem(itemToAdd, 1));
+        }
+
+        public void MarkQuestCompleted(Quest quest)
+        {
+            // Find the quest in the player's quest list
+            foreach (PlayerQuest pq in Quests)
+            {
+                if (pq.Details.ID == quest.ID)
+                {
+                    // Mark it as completed
+                    pq.IsCompleted = true;
+
+                    return; // We found the quest, and marked it complete, so get out of this function
+                }
+            }
+        }
+
         #endregion
 
     }//class player
